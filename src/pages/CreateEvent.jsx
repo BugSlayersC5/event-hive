07385c47701983daaf10d0dad7353e41391e7379
jessicaaning <jersycaowusu@gmail.com>
2025-06-15 22:49:38 +0000

    working on view event page

--- src/pages/CreateEvent.jsx
+++ src/pages/CreateEvent.jsx
@@ -5,10 +5,10 @@ export default function CreateEvent() {
     return (
         <>
             <Navbar />
-            <section className="flex flex-col items-center bg-gray-200 justify-center w-[80%] p-4 gap-6 mx-auto">
+            <section className="flex flex-col items-center bg-gray-100 justify-center w-[100%] p-4 gap-6 mx-auto">
                 <h1 className="text-center font-bold text-black text-4xl">Welcome to the Create Event Page</h1>
 
-                <div className="mb-4 bg-gray w-80">
+                <div className="mb-4 bg-gray w-[60%]">
                     <label htmlFor="event name" className="block mb-2 text-sm font-medium text-gray-700">
                         Event Title
                     </label>
@@ -17,7 +17,7 @@ export default function CreateEvent() {
                         placeholder="Enter the title of your event"
                         className="w-full p-3 rounded-md bg-white text-gray-700 placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-blue-500"
                     />
-                    <div className="mb-4 w-80">
+                    <div className="mb-4 bg-gray w-[100%]">
                         <label htmlFor="venue" className="block mb-2 text-sm font-medium text-gray-700">
                             Event Venue
                         </label>
@@ -29,19 +29,19 @@ export default function CreateEvent() {
                     </div>
 
                     <div className="grid grid-cols-2 gap-4">
-                        <div>
+                        <div className="w-[90%]">
                             <label htmlFor="start time" className="block mb-2 text-sm font-medium  text-gray-700 placeholder-gray-200">Start Time</label>
                             <input type="time"
                                 placeholder="Enter start time"
                                 name="start time"
                                 id="start time"
-                                className="w-full bg-white p-3 rounded-md text-gray-700 placeholder-gray-400 focus-outline-none focus-ring-2 focus-ring-blue-500"
+                                className="w-full bg-white p-3 mb-4 rounded-md text-gray-700 placeholder-gray-400 focus-outline-none focus-ring-2 focus-ring-blue-500"
                             />
                         </div>
                         <div>
                             <label htmlFor="End time" className="block mb-2 text-sm font-medium  text-gray-700 placeholder-gray-200">End Time</label>
                             <input type="time"
-                                placeholder="enter end Time"
+                                placeholder="enter end time"
                                 name="enter end time"
                                 className="w-full bg-white p-3 rounded-md text-gray-700 placeholder-gray-400 focus-outline-none focus-ring-2 focus-ring-blue-500"
                             />
@@ -49,21 +49,48 @@ export default function CreateEvent() {
 
                         <div>
                             <label htmlFor="start date">Start Date</label>
-                            <input type="date" 
-                                   placeholder="enter"
-                                   id="start date"
-                                   name="start date"
-                                   className="w-full bg-white p-3 rounded-md text-gray-700 placeholder-gray-400 focus-outline focus-ring-2 focus-ring-blue-500"/>
+                            <input type="date"
+                                placeholder="enter date"
+                                id="start date"
+                                name="start date"
+                                className="w-full bg-white p-3 mb-6 rounded-md text-gray-700 placeholder-gray-400 focus-outline focus-ring-2 focus-ring-blue-500" />
                         </div>
+                        <div>
+                            <label htmlFor="end date">End Date</label>
+                            <input type="date"
+                                placeholder="enter date"
+                                id="end date"
+                                name="end date"
+                                className="w-full bg-white p-3 mb-4 rounded-md text-gray-700 placeholder-gray-400 focus-outline focus-ring-2 focus-ring-blue-500" />
+                        </div>
+
+                       
                     </div>
-
-
-
-
                 </div>
 
+                <section className="flex flex-col-reverse items-center justify-center mx-auto gap-6 mt-15">
+                    <div>
+                        <h2 className="text-2xl font-bold ">Event Discriptionn</h2>
+                        <h2 className=" mt-10 mx-5">Event Image</h2>
 
+                        <div className="bg-gray-300 w-2xl h-50 mt-5  p-4 ">
+                        
+                    </div>
+
+                     <div>
+                        <h2 className=" font mt-30 text start">Event Description</h2>
+                          <div className="bg-white w-2xl h-50 mt-5  p-4 "></div>
+                    </div>
+                    </div>
+
+                   
+
+                    
+
+                </section>
             </section>
+
+
         </>
     );
 }
--- src/pages/ViewEvent.jsx
+++ src/pages/ViewEvent.jsx
@@ -3,10 +3,16 @@ import Footer from "../components/Footer";
 import EventDetail from "../components/EventDetail";
 import SimilarEvents from "../components/SimilarEvents";
 
+
 export default function ViewEvent() {
     return (
         <>
             <Navbar />
+            <section className="bg-[url('../assets/images/event-img.jpg')] bg-cover bg-center h-64 w-full">
+            <h1>Dream world wide in jakatra</h1>
+            <h2>IIIT</h2>
+
+            </section>
             <EventDetail />
             <SimilarEvents />
             <Footer />
