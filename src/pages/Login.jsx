0a5a8c55d36123b9b5ac2ae204db3e0a6aefeb78
Aya-Kukua <ayakukua001@gmail.com>
2025-06-15 23:26:23 +0000

    WIP: login and register pages. Replaced title in index.html

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,63 @@
+import { useState } from "react";
+import GoogleIcon from "../assets/images/google-icon.png"
+import loginImage from "../assets/images/login.png"
+
 export default function Login() {
+
+    const [form] = useState({ name: '', password: '', confirmPassword: '' });
     return (
-        <div>
-            <h1>Welcome to the Login Page</h1>
+        <div className="flex h-screen font-sans">
+            <form className="w-1/2 min-h-screen flex flex-col justify-center items-center bg-gray-50 px-16 space-y-6">
+
+                <h2 className="text-2xl text-heading text-purple-600 font-semibold">Event <span className="text-black">Hive</span></h2>
+                <h1 className="text-2xl text-big-heading font-sans mt-4 mb-8">Sign Up to Event Hive</h1>
+                <div className="w-full">
+                    <label htmlFor="email" className="text-black font-bold text-2xl uppercase"> Your Email</label>
+                    <input
+                        name="email"
+                        value={form.name}
+                        placeholder="Enter your email"
+                        className="w-full mb-4 p-3 rounded border border-gray-300 focus:outline-none focus:ring-2 focus:ring-purple-400"
+                    />
+                </div>
+
+                <div className="w-full">
+                    <label htmlFor="Password" className="text-black font-bold text-2xl uppercase">Password</label>
+                    <input
+                        type="password"
+                        name="password"
+                        value={form.password}
+                        placeholder="Enter your password"
+                        className="w-full mb-4 p-3 rounded border border-gray-300 focus:outline-none focus:ring-2 focus:ring-purple-400"
+                    />
+                </div>
+
+
+                <button className="w-1/3 bg-purple-600 text-white font-semibold py-3 rounded hover:bg-purple-700 transition mb-4">
+                    Sign Up
+                </button>
+
+                <div className="mb-4 text-gray-500">Or</div>
+
+                <button className="w-1/2 border border-gray-300 flex items-center justify-center py-3 rounded hover:bg-gray-100">
+                    <img src={GoogleIcon} alt="Google" className="w-5 h-5 mr-2" />
+                    Sign up with Google
+                </button>
+            </form>
+
+
+            <div  style={{ backgroundImage: `url(${loginImage})`, minHeight: '500px' }} className="w-1/2 bg-cover bg-center relative  ">
+            
+                <div className="absolute inset-0 bg-black bg-opacity-50 flex flex-col items-center justify-center text-white p-8">
+                    <h1 className="text-4xl font-bold mb-4">Hello Friend</h1>
+                    <p className="mb-6 text-center">To keep connected with us provide us with your information</p>
+                    <button className="bg-gray-300 text-black font-medium py-2 px-6 rounded-md">Sign In</button>
+                </div>
+            </div>
+
         </div>
+
+
+
     );
 }
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,22 +1,15 @@
-import RegisterImage from "../assets/images/register.png"
 import { useState } from "react";
 import GoogleIcon from "../assets/images/google-icon.png"
 
 export default function Register() {
 
-    const [form, setForm] = useState({ name: '', password: '', confirmPassword: '' });
+    const [form] = useState({ name: '', password: '', confirmPassword: '' });
 
-    const handleChange = (e) => {
-        setForm({
-            ...form,
-            [e.target.name]: e.target.value
-        });
-    };
 
     return (
         <div className="flex h-screen font-sans">
             {/* Left side */}
-            <div className="w-1/2 bg-cover bg-center relative" style={{ RegisterImage: `url(${RegisterImage})` }}>
+            <div className="w-1/2 bg-cover bg-center relative bg-[url(../assets/images/register.png)]">
                 <div className="absolute inset-0 bg-black bg-opacity-50 flex flex-col items-center justify-center text-white p-8">
                     <h1 className="text-4xl font-bold mb-4">Welcome back</h1>
                     <p className="mb-6 text-center">To keep connected with us provide us with your information</p>
@@ -25,55 +18,61 @@ export default function Register() {
             </div>
 
             {/* Right side */}
-            <div className="w-1/2 flex flex-col justify-center items-center bg-gray-50 px-16">
-                <h2 className="text-sm text-purple-600 font-semibold">Event <span className="text-black">Hive</span></h2>
-                <h1 className="text-2xl font-bold mt-4 mb-8">Sign Up to Event Hive</h1>
+           <form className="w-1/2 flex flex-col justify-center items-center bg-gray-50 px-16 space-y-6">
 
-                <input
-                    name="name"
-                    value={form.name}
-                    onChange={handleChange}
-                    placeholder="Enter your name"
-                    className="w-full mb-4 p-3 rounded border border-gray-300 focus:outline-none focus:ring-2 focus:ring-purple-400"
-                />
+                <h2 className="text-2xl text-heading text-purple-600 font-semibold">Event <span className="text-black">Hive</span></h2>
+                <h1 className="text-2xl text-big-heading font-sans mt-4 mb-8">Sign Up to Event Hive</h1>
+                <div className="w-full">
+                    <label htmlFor="name" className="text-black font-bold text-2xl uppercase"> Your Name</label>
+                    <input
+                        name="name"
+                        value={form.name}
+                        placeholder="Enter your name"
+                        className="w-full mb-4 p-3 rounded border border-gray-300 focus:outline-none focus:ring-2 focus:ring-purple-400"
+                    />
+                </div>
+                <div className="w-full">
+                    <label htmlFor="Username" className="text-black font-bold text-2xl uppercase"> Username</label>
+                    <input
+                        name="name"
+                        value={form.name}
+                        placeholder="Create user name"
+                        className="w-full mb-4 p-3 rounded border border-gray-300 focus:outline-none focus:ring-2 focus:ring-purple-400"
+                    />
+                </div>
 
-                <input
-                    name="name"
-                    value={form.name}
-                    onChange={handleChange}
-                    placeholder="Enter your name"
-                    className="w-full mb-4 p-3 rounded border border-gray-300 focus:outline-none focus:ring-2 focus:ring-purple-400"
-                />
+                <div className="w-full">
+                    <label htmlFor="Password" className="text-black font-bold text-2xl uppercase">Password</label>
+                    <input
+                        type="password"
+                        name="password"
+                        value={form.password}
+                        placeholder="Enter your password"
+                        className="w-full mb-4 p-3 rounded border border-gray-300 focus:outline-none focus:ring-2 focus:ring-purple-400"
+                    />
+                </div>
+                <div className="w-full">
+                    <label htmlFor="confirm password" className="text-black font-bold text-2xl uppercase">Confirm password</label>
+                    <input
+                        type="password"
+                        name="confirmPassword"
+                        value={form.confirmPassword}
+                        placeholder="Confirm your password"
+                        className="w-full mb-6 p-3 rounded border border-gray-300 focus:outline-none focus:ring-2 focus:ring-purple-400"
+                    />
+                </div>
 
-                <input
-                    type="password"
-                    name="password"
-                    value={form.password}
-                    onChange={handleChange}
-                    placeholder="Enter your password"
-                    className="w-full mb-4 p-3 rounded border border-gray-300 focus:outline-none focus:ring-2 focus:ring-purple-400"
-                />
-
-                <input
-                    type="password"
-                    name="confirmPassword"
-                    value={form.confirmPassword}
-                    onChange={handleChange}
-                    placeholder="Confirm your password"
-                    className="w-full mb-6 p-3 rounded border border-gray-300 focus:outline-none focus:ring-2 focus:ring-purple-400"
-                />
-
-                <button className="w-full bg-purple-600 text-white font-semibold py-3 rounded hover:bg-purple-700 transition mb-4">
+                <button className="w-1/3 bg-purple-600 text-white font-semibold py-3 rounded hover:bg-purple-700 transition mb-4">
                     Sign Up
                 </button>
 
                 <div className="mb-4 text-gray-500">Or</div>
 
-                <button className="w-full border border-gray-300 flex items-center justify-center py-3 rounded hover:bg-gray-100">
+                <button className="w-1/2 border border-gray-300 flex items-center justify-center py-3 rounded hover:bg-gray-100">
                     <img src={GoogleIcon} alt="Google" className="w-5 h-5 mr-2" />
                     Sign up with Google
                 </button>
-            </div>
+           </form>
         </div>
     );
 }
