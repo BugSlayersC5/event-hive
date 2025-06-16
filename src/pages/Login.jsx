8147a4d68658e045dd11a1a1ab2c474d07e41983
Aya-Kukua <ayakukua001@gmail.com>
2025-06-16 10:41:17 +0000

    Worked on linking the Sign up/in buttons

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import GoogleIcon from "../assets/images/google-icon.png"
 import loginImage from "../assets/images/login.png"
+import { Link } from "react-router";
 
 export default function Login() {
 
@@ -10,7 +11,7 @@ export default function Login() {
             <form className="w-1/2 min-h-screen flex flex-col justify-center items-center bg-gray-50 px-16 space-y-6">
 
                 <h2 className="text-2xl text-heading text-purple-600 font-semibold">Event <span className="text-black">Hive</span></h2>
-                <h1 className="text-2xl text-big-heading font-sans mt-4 mb-8">Sign Up to Event Hive</h1>
+                <h1 className="text-2xl text-big-heading font-sans mt-4 mb-8">Sign In to Event Hive</h1>
                 <div className="w-full">
                     <label htmlFor="email" className="text-black font-bold text-sm uppercase"> Your Email</label>
                     <input
@@ -34,14 +35,14 @@ export default function Login() {
 
 
                 <button className="w-1/3 bg-purple-600 text-white font-semibold py-3 rounded hover:bg-purple-700 transition mb-4">
-                    Sign Up
+                    Sign In
                 </button>
 
                 <div className="mb-4 text-gray-500">Or</div>
 
                 <button className="w-1/2 border border-gray-300 flex items-center justify-center py-3 rounded hover:bg-gray-100">
                     <img src={GoogleIcon} alt="Google" className="w-5 h-5 mr-2" />
-                    Sign up with Google
+                    Sign In with Google
                 </button>
             </form>
 
@@ -56,7 +57,7 @@ export default function Login() {
                 <div className="absolute inset-0 flex flex-col items-center justify-center text-white p-8  rounded-lg shadow-lg border border-opacity-10 border-white">
                     <h1 className="text-4xl font-bold mb-4">Hello Friend</h1>
                     <p className="mb-6 text-center">To keep connected with us, provide us with your information</p>
-                    <button className="bg-white text-black font-medium py-2 px-6 rounded-md hover:bg-gray-200 transition duration-300">Sign In</button>
+                    <Link to={'/register'} className="bg-white text-black font-medium py-2 px-6 rounded-md hover:bg-gray-200 transition duration-300">Sign up</Link>
                 </div>
             </div>
 
